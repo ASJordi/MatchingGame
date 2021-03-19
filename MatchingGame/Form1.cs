bc9d9d79f1e2d7dd5ce50b02f0834a60ca5dd80e
Jordi <48893030+ImJrd@users.noreply.github.com>
2021-03-19 10:49:31 -0600

    Asignar icono aleatorio a etiqueta

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -24,9 +24,38 @@ namespace MatchingGame
             "b", "b", "v", "v", "w", "w", "z", "z"
         };
 
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                // The statements you want to execute 
+                // for each label go here
+                // The statements use iconLabel to access 
+                // each label's properties and methods
+
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
         }
     }
 }
